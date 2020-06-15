04b4d57a6b7df218af642c929bbc52f72a1818b2
djordjebatic <djordjebatic@gmail.com>
2020-06-15 21:08:35 +0200

    front

---
+++ WebContent/precache-manifest.a58ea61ff4a11f289d59e00c1f67099d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e8bccfc78a9b4f0c329f15da3561da",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9207e567ddd6d9cf5198",
+    "url": "./static/css/main.11ce02c2.chunk.css"
+  },
+  {
+    "revision": "7bdf32a45ade1b352c4f",
+    "url": "./static/js/2.a314f72f.chunk.js"
+  },
+  {
+    "revision": "9207e567ddd6d9cf5198",
+    "url": "./static/js/main.099f609d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
